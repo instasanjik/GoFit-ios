0bc8a2bcca04ec9e092ed8e87a6c6f08d3499389
instasanjik <koshkarbayev.07@gmail.com>
2022-06-06 11:39:54 +0600

    feat: create welcome page

--- GoFit/ViewControllers/WelcomeViewController.swift
+++ GoFit/ViewControllers/WelcomeViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
-
+final class WelcomeViewController: UIViewController {
+    @IBOutlet weak var taglineLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        setUpObjects()
+    }
+    
+    private func setUpObjects(){
+        taglineLabel.textColor = ColorPalette.gray1
+        startButton.layer.cornerRadius = startButton.frame.width/10.0
     }
 
 
