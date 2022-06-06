6dbbcb277cf3ef4a37b718665e4fc0ed1c5f57ab
instasanjik <koshkarbayev.07@gmail.com>
2022-06-06 11:39:27 +0600

    feat: add color palette

---
+++ GoFit/Static/ColorPalette.swift
@@ -0,0 +1,13 @@
+//
+//  ColorPalette.swift
+//  GoFit
+//
+//  Created by Sanzhar Koshkarbayev on 06.06.2022.
+//
+
+import Foundation
+import UIKit
+
+class ColorPalette{
+    static let gray1 = UIColor(red: 0.482, green: 0.435, blue: 0.447, alpha: 1)
+}
