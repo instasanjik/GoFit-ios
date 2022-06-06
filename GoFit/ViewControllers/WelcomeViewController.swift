51472194e94457f83bbc69f9b793a546f6aa877c
instasanjik <koshkarbayev.07@gmail.com>
2022-06-06 10:47:05 +0600

    refactor: sort files

--- GoFit/ViewControllers/WelcomeViewController.swift
+++ GoFit/ViewControllers/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  GoFit
 //
 //  Created by Sanzhar Koshkarbayev on 06.06.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
